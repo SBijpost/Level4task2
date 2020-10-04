385c0cb638d7b0656fe0aaf3757bd3fdc9c603ea
root <sembijpost@gmail.com>
2020-10-04 20:29:12 +0200

    klaar

---
+++ app/src/main/java/com/sem/level4task1/Product.kt
@@ -0,0 +1,20 @@
+package com.sem.level4task1
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "productTable")
+data class Product(
+
+    @ColumnInfo(name = "product")
+    var productName: String,
+
+    @ColumnInfo(name = "quantity")
+    var quantity: Short?,
+
+    @PrimaryKey(autoGenerate = true)
+    @ColumnInfo(name = "id")
+    var id: Long? = null
+
+)
